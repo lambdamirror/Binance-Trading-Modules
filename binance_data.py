7dd4e3a6c753f4f420c1125195ec57d6b16abe64
lambdamirror <lambdamirror@users.noreply.github.com>
2020-08-30 00:25:23 +1000

    update

--- binance_data.py
+++ binance_data.py
@@ -8,87 +8,71 @@ Created on Wed Mar 25 15:32:17 2020
 import warnings
 warnings.simplefilter("ignore")
 
-import time
+import time, sys
 import numpy as np
 import pandas as pd
 from datetime import datetime
+from binancepy import MarketData
 
-from tqdm import tqdm
-from binance.client import Client
-from ultilities import barstr, timestr
-
+min_in_candle ={'1m':1, '3m':3, '5m':5, '10m':10, '15m':15, '30m': 30, \
+                '1h':60, '4h':240, '6h':360, '12h':720, '1d':1440}     
 start_time = time.time()
 
-def tick_bars(df, m):
-    t_df = pd.DataFrame(columns=['_t', '_o','_h', '_l', '_c', '_v'])
-    low_, high_, open_ = 1e6, 0, df['_p'].iloc[0]
-    t_ = df['_t'].iloc[0]
-    ts, vol_ = 0, 0
-    for i, x in enumerate(tqdm(df['_p'])):
-        ts += df['_v'].iloc[i]*df['_p'].iloc[i]
-        vol_ += df['_v'].iloc[i]
-        if df['_p'].iloc[i] < low_: low_ = df['_p'].iloc[i]
-        if df['_p'].iloc[i] > high_: high_ = df['_p'].iloc[i]
-        if ts >= m:
-            t_df = t_df.append({'_t': t_,\
-                                '_o': open_, \
-                                '_h': high_, \
-                                '_l': low_, \
-                                '_c': df['_p'].iloc[i], \
-                                '_v': vol_ }, \
-                                ignore_index=True)
-            t_ = df['_t'].iloc[i]
-            [low_, high_, open_] = [df['_p'].iloc[i]]*3
-            ts,  vol_ = 0, 0
-            continue
-#    t_df = t_df.set_index('_t')
-    return t_df
+def klns_to_df(market_data, feats):
+    '''
+    Add columns' name to dataFrame received from market and select the columns in feats
+    
+    feats -> ['_o', '_h', '_l', '_c', '_v']
+    '''
+    fts = list(str(f) for f in feats)
+    _df = pd.DataFrame(market_data, columns = ['_t', '_o', '_h', '_l', '_c', '_v', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
+    _df[['_o', '_h', '_l', '_c', '_v']] = _df[['_o', '_h', '_l', '_c', '_v']].astype(float)
+    return _df[fts]
 
-def get_ticks(symbol, start_time, filepath):    
-    if not isinstance(start_time, int): start_time = int(pd.Timestamp(datetime.strptime(start_time, '%d %b %Y %H:%M:%S')).value/10**6)
-    print("\nProcessing aggTrades for  %s from %s \n" % (symbol, timestr(start_time, end='s')))
-    agg_trades = client.aggregate_trade_iter(symbol=symbol, start_str=start_time)
-    trade_df = pd.DataFrame(list(agg_trades)).drop(['a', 'f','l','m','M'], axis=1)
-    trade_df = trade_df.rename(columns={"p": "_p", "q": "_v", "T": "_t"})
-    trade_df['_p'] = trade_df['_p'].astype(float)
-    trade_df['_v'] = trade_df['_v'].astype(float)
-    rawfile = filepath + "aggtrades-%s.csv" % (symbol)
-    trade_df.to_csv(rawfile)
-    return trade_df
-
-def get_klines(symbol, kl_size, start_time, end_time, filepath):
-    if isinstance(start_time, int): start_time = pd.to_datetime(start_time, unit='ms').strftime('%d %b %Y %H:%M:%S')
-    if isinstance(end_time, int): end_time = pd.to_datetime(end_time, unit='ms').strftime('%d %b %Y %H:%M:%S')
-    print("\nProcessing data sub {} for {} from {} to {}\n".format(kl_size, symbol, start_time, end_time))
-    klns = client.get_historical_klines(symbol, kl_size, start_time, end_time)
-    kln_df = pd.DataFrame(klns, columns = ['_t', '_o', '_h', '_l', '_c', '_v','close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
-    kln_df = kln_df.drop(['close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'], axis=1) 
-    klnfile = filepath + "-%s-%s.csv" % (symbol, kl_size)
-    kln_df.to_csv(klnfile)
+def candle_no_limit(mkData, interval, startTime, endTime):
+    '''
+    Return dataFrame of mkData from startTime to endTime
+    '''
+    min_in_candle ={'1m':1, '3m':3, '5m':5, '10m':10, '15m':15, '30m': 30, \
+                    '1h':60, '4h':240, '6h':360, '12h':720, '1d':1440}
+    
+    if not isinstance(startTime, int): 
+        startTime = int(pd.Timestamp(datetime.strptime(startTime, '%d %b %Y %H:%M:%S')).value/10**6)
+    if not isinstance(endTime, int): 
+        endTime = int(pd.Timestamp(datetime.strptime(endTime, '%d %b %Y %H:%M:%S')).value/10**6)    
+    sub_period = int((endTime - startTime)/(min_in_candle[interval]*60*1000))
+    sub_start = startTime
+    kln_df = None
+    while sub_period > 0:
+        numklns = min(sub_period, 500)
+        market_df = mkData.candles_data(interval=interval, startTime=sub_start, limit=numklns)
+        sub_df = klns_to_df(market_df, ['_t', '_o', '_h', '_l', '_c', '_v'])
+        if kln_df is None:
+            kln_df = pd.DataFrame(sub_df)
+        else:
+            kln_df = kln_df.append(pd.DataFrame(sub_df), ignore_index=True)
+        sub_period -= numklns
+        sub_start += numklns*min_in_candle[interval]*60*1000
     return kln_df
 
-### API
-binance_api_key = ''    #Enter your own API-key here
-binance_api_secret = '' #Enter your own API-secret here
-client = Client(api_key=binance_api_key, api_secret=binance_api_secret)
+def main(args):
+    '''
+    Main function to call candle_no_limit()
+    '''
+    filepath = ''
+    slist = [ 'BTCUSDT', 'ETHUSDT', 'BCHUSDT']
+    for symbol in slist:
+        data = MarketData(exchange='perpetual', testnet=False, symbol=symbol)
+        interval = '3m'
+        t_start = '01 Jul 2020 00:00:00' 
+        t_end = '03 Jul 2020 00:00:00'
 
-filepath_tik = '' #'data/ticks/' 
-filepath_kln = '' #'data/klines/'
-slist = [ 'BTCUSDT', 'ETHUSDT', 'BCHUSDT', 'LINKUSDT', 'LTCUSDT', 'XTZUSDT' ]  #'BNBUSDT', 'LINKUSDT', 'ETCUSDT', 'XTZUSDT', 'TRXUSDT', 'XRPUSDT', 'EOSUSDT'
-start_time = time.time()
-kl_size = '1m'
-# t_start, t_end = '10 May 2020 00:00:00', '11 May 2020 00:00:00'
-period = 6*60
-t_end = int(client.get_server_time()['serverTime'])
-t_start =  t_end - period*60*1000
-for symbol in slist:
-   df = get_klines(symbol, kl_size, t_start, t_end, filepath_kln)
-   t_df = get_ticks(symbol, t_start, filepath_tik)
-   dollar_bin = (t_df['_p'] @ t_df['_v'])/(1000*(24*60)/5)
-   dollar_bin = int(dollar_bin)*1000
-   bar_df = tick_bars(t_df, dollar_bin)
-   bar_df.to_csv(filepath_tik + "tick-%s.csv" % (symbol))
+        klns = candle_no_limit(data, interval=interval, startTime=t_start, endTime=t_end)
+        klnfile = filepath + "%s-%s.csv" % (symbol, interval)
+        klns.to_csv(klnfile)
 
-print('\n' + barstr(text='Elapsed time = {} seconds'.format(round(time.time()-start_time,2))))
-print(barstr(text="", space_size=0))
+    print('\n\nElapsed time = ' + str(time.time()-start_time))
+    print("\n#################### \n")
 
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- binancepy.py
+++ binancepy.py
@@ -6,10 +6,13 @@ Created on Sat Mar 24 00:16:07 2020
 
 CREDIT: This is a modified version from the original module of morozdima.
         For more information, please visit: https://github.com/morozdima/BinanceFuturesPy
-        
-Docmentation of Binance Futures Websocket can be found here: https://binance-docs.github.io/apidocs/futures/en
+
+Docmentation can be found here:
+    - Binance Futures: https://binance-docs.github.io/apidocs/futures/en
+    - Binance Delivery: https://binance-docs.github.io/apidocs/delivery/en
 """
 
+from sys import stdout
 import time
 
 import numpy as np
@@ -20,29 +23,47 @@ import urllib
 import json
 import hmac
 import hashlib
-     
-#%%%%     
+
+#%%%%
 class MarketData:
-    
+
     def __init__(self,
+                 exchange: str = 'perpetual',
                  testnet: bool = False,
                  symbol: str = 'btcusdt'):
 
         '''
-        
+
+        To use Delivery Binance Futures API  -> exchange = 'delivery'
+
         To use TESTNET Binance Futures API  -> testnet = True
-        
+
         To change currency pair             -> symbol = 'ethusdt'
-        
+
+        To change interval                  -> interval = '5m'
+        (m -> minutes
+         h -> hours
+         d -> days
+         w -> weeks
+         M -> months;
+
+        Valid values: [1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M])
+
         '''
 
-        if testnet == True:
-            self.http_way = 'http://testnet.binancefuture.com/fapi/v1/'
-        else:
-            self.http_way = 'http://fapi.binance.com/fapi/v1/'
-        
-        self.wss_way = 'wss://fstream.binance.com/ws/'
-        self.symbol = symbol.lower()
+        self.exchange = exchange
+        if self.exchange=='perpetual':
+            if testnet == True:
+                self.http_way = 'http://testnet.binancefuture.com/fapi/v1/'
+            else:
+                self.http_way = 'http://fapi.binance.com/fapi/v1/'
+            self.symbol = symbol.lower()
+        elif self.exchange=='delivery':
+            if testnet == True:
+                self.http_way = 'http://testnet.binancefuture.com/dapi/v1/'
+            else:
+                self.http_way = 'http://dapi.binance.com/dapi/v1/'
+            self.symbol = symbol.upper()
 
     def ping(self):
         return requests.get(f'{self.http_way}ping').json()
@@ -66,7 +87,7 @@ class MarketData:
                 return dict([])
             else:
                 return r
-    
+
     def recent_trades(self, limit: int = 500):
         '''
         To change limit -> limit = 1000
@@ -80,14 +101,14 @@ class MarketData:
                 return dict([])
             else:
                 return r
-    
+
     def historical_trades(self, limit: int = 500):
         '''
         To change limit -> limit = 1000
         (max 1000)
         '''
         return requests.get(f'{self.http_way}historicalTrades?symbol={self.symbol}&limit={limit}').json()
-    
+
     def aggregate_trades(self,
                          fromId: int = None,
                          startTime: int = None,
@@ -96,7 +117,7 @@ class MarketData:
         '''
         To change limit                     ->  limit = 1000
         (max 1000)
-        
+
         To use fromId                       ->  fromId = 1231
         To use start time and end time      ->  startTime = 1573661424937
                                             ->  endTime = 1573661428706
@@ -111,45 +132,51 @@ class MarketData:
         '''
         To change interval                  ->  interval = '5m'
         (Valid values: [1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M])
-        
+
         To use limit                        ->  limit = 1231
         (Default 500; max 1500)
-        
+
         To use start time and end time      ->  startTime = 1573661424937
                                             ->  endTime = 1573661428706
         '''
-        return requests.get(f'{self.http_way}klines?symbol={self.symbol}&interval={interval}&startTime={startTime}&endTime={endTime}&limit={limit}').json()
-    
+        params = {  'symbol' : self.symbol,
+                    'interval' : interval,
+                    'startTime' : startTime,
+                    'endTime' : endTime,
+                    'limit' : limit }
+        query = urllib.parse.urlencode(dict([(k, v) for (k, v) in iter(params.items()) if v]))
+        return requests.get(f'{self.http_way}klines?{query}').json()
+
     def mark_price(self):
         return requests.get(f'{self.http_way}premiumIndex?symbol={self.symbol}').json()
-    
+
     def funding_rate(self,
                      startTime: int = None,
                      endTime: int = None,
                      limit: int = 100):
         '''
         To change limit                     ->  limit = 1000
-        (max 1096)
-        
+        (max 1000)
+
         To use start time and end time      ->  startTime = 1573661424937
                                             ->  endTime = 1573661428706
         '''
         return requests.get(f'{self.http_way}klines?symbol={self.symbol}&startTime={startTime}&endTime={endTime}&limit={limit}').json()
-    
+
     def ticker_price_24h(self,
                          symbol: bool = False):
         if symbol is True:
             return requests.get(f'{self.http_way}ticker/24hr?symbol={self.symbol}').json()
         else:
             return requests.get(f'{self.http_way}ticker/24hr').json()
-    
+
     def ticker_price_symbol(self,
                             symbol: bool = False):
         if symbol is True:
             return requests.get(f'{self.http_way}ticker/price?symbol={self.symbol}').json()
         else:
             return requests.get(f'{self.http_way}ticker/price').json()
-    
+
     def ticker_orderbook_symbol(self,
                                 symbol: bool = False):
         if symbol is True:
@@ -157,34 +184,60 @@ class MarketData:
         else:
             return requests.get(f'{self.http_way}ticker/bookTicker').json()
 
+    def open_interest(self):
+        return requests.get(f'{self.http_way}openInterest').json()
+
+    def open_interest_statistics(self,
+                                 period : str = '5m',
+                                 startTime : int = None,
+                                 endTime : int = None,
+                                 limit : int = 500):
+        if self.exchange=='perpetual':
+            base_url = self.http_way.replace('fapi/v1/', 'futures/data/')
+            params = {  'symbol' : self.symbol,
+                        'period' : period,
+                        'startTime' : startTime,
+                        'endTime' : endTime,
+                        'limit' : limit }
+            query = urllib.parse.urlencode(dict([(k, v) for (k, v) in iter(params.items()) if v]))
+            return requests.get(f'{base_url}openInterestHist?{query}').json()
+        else:
+            return {}
 
 #%%%%
 class Client:
     def __init__(self,
                  api_key: str,
                  sec_key: str,
+                 exchange: str = 'perpetual',
                  testnet: bool = False):
         '''
         In any case you must give your API key and API secret to work with Client
-        
+
+        To use Delivery Binance Futures API  -> exchange = 'delivery'
+
         To use TESTNET Binance Futures API  -> testnet = True
         '''
-        
+
         self.api_key = api_key
         self.sec_key = sec_key
         self.X_MBX_APIKEY = {"X-MBX-APIKEY": self.api_key}
-        
-        if testnet == True:
-            self.http_way = 'http://testnet.binancefuture.com/fapi/v1/'
-            self.wss_way = 'wss://stream.binancefuture.com/ws/'
-        else:
-            self.http_way = 'http://fapi.binance.com/fapi/v1/'
-            self.wss_way = 'wss://fstream.binance.com/ws/'
-    
-    
-    '''
-    Implied REST method: GET / POST / PUT / DELETE
-    '''
+
+        if exchange=='perpetual':
+            if testnet == True:
+                self.http_way = 'http://testnet.binancefuture.com/fapi/v1/'
+                self.wss_way = 'wss://stream.binancefuture.com/ws/'
+            else:
+                self.http_way = 'http://fapi.binance.com/fapi/v1/'
+                self.wss_way = 'wss://fstream.binance.com/ws/'
+        elif exchange=='delivery':
+            if testnet == True:
+                self.http_way = 'http://testnet.binancefuture.com/dapi/v1/'
+                self.wss_way = 'wss://dstream.binancefuture.com/ws/'
+            else:
+                self.http_way = 'http://dapi.binance.com/dapi/v1/'
+                self.wss_way = 'wss://dstream.binance.com/ws/'
+
     def _get_request(self,
                       req,
                       query):
@@ -192,7 +245,7 @@ class Client:
                                            query=query,
                                            signature=self.get_sign(query=query)),
                           headers=self.X_MBX_APIKEY)
-        
+
         try:
             return r.json()
         except:
@@ -200,7 +253,7 @@ class Client:
                 return dict([])
             else:
                 return r
-    
+
     def _post_request(self,
                       req,
                       query):
@@ -208,7 +261,7 @@ class Client:
                                            query=query,
                                            signature=self.get_sign(query=query)),
                           headers=self.X_MBX_APIKEY)
-        
+
         try:
             return r.json()
         except:
@@ -216,7 +269,7 @@ class Client:
                 return dict([])
             else:
                 return r
-    
+
     def _delete_request(self,
                       req,
                       query):
@@ -232,7 +285,7 @@ class Client:
                 return dict([])
             else:
                 return r
-    
+
     def _put_request(self,
                       req,
                       query):
@@ -248,6 +301,7 @@ class Client:
             else:
                 return r
 
+#    @staticmethod
     def timestamp(self):
         r = requests.get(f'{self.http_way}time')
         try:
@@ -260,11 +314,11 @@ class Client:
                 return r
 
     def get_sign(self, query):
-        
+
         return hmac.new(self.sec_key.encode('utf-8'), query.encode('utf-8'), hashlib.sha256).hexdigest()
 
     def request_url(self, req, query, signature):
-        
+
         return self.http_way + req + query + '&signature=' + signature
 
     def new_order(self,
@@ -283,7 +337,7 @@ class Client:
                   workingType: str = None):
         '''
         POST
-        
+
         Choose side:                SELL or BUY
         Choose quantity:            0.001
         Choose price:               7500
@@ -292,9 +346,9 @@ class Client:
         To change order type    ->  orderType = 'MARKET'/'LIMIT'/'TRAILING_STOP_MARKET'
         To change time in force ->  timeInForce = 'GTC'/'IOC'/'FOK'
         '''
-        
+
         req = 'order?'
-        
+
         querystring = {'symbol' : symbol.lower(),
                        'side' : side,
                        'positionSide': positionSide,
@@ -312,12 +366,12 @@ class Client:
             querystring['stopPrice'] = stopPrice
         if workingType is not None:
             querystring['workingType'] = workingType
-        if activationPrice is not None: 
+        if activationPrice is not None:
             querystring['activationPrice'] = activationPrice
-        if callbackRate is not None: 
+        if callbackRate is not None:
             querystring['callbackRate'] = callbackRate
         querystring['timestamp'] = self.timestamp()
-        
+
         querystring = urllib.parse.urlencode(querystring)
 
         return self._post_request(req, querystring)
@@ -325,20 +379,20 @@ class Client:
     def query_order(self, symbol, orderId):
         '''
         GET
-        
+
         Choose orderId: 156316486
         '''
         req = 'order?'
         querystring = urllib.parse.urlencode({'symbol' : symbol.lower(),
                                               'orderId' : orderId,
                                               'timestamp' : self.timestamp()})
-    
+
         return self._get_request(req, querystring)
 
     def cancel_order(self, symbol, orderId):
         '''
         DELETE
-        
+
         Choose orderId: 156316486
         '''
         req = 'order?'
@@ -396,11 +450,11 @@ class Client:
         querystring = urllib.parse.urlencode({'timestamp' : self.timestamp()})
 
         return self._get_request(req, querystring)
-    
+
     def change_position_mode(self, dualSide='false'):
         '''
         POST
-        
+
         To change position mode: "true": Hedge Mode; "false": One-way Mode
         '''
         req = 'positionSide/dual?'
@@ -412,7 +466,7 @@ class Client:
     def change_leverage(self, symbol, leverage):
         '''
         POST
-        
+
         To change leverage -> leverage = 25
         (from 1 to 125 are valid values)
         '''
@@ -427,7 +481,7 @@ class Client:
         '''GET'''
         req = 'positionRisk?'
         querystring = urllib.parse.urlencode({'timestamp' : self.timestamp()})
-        
+
         return self._get_request(req, querystring)
 
     def trade_list(self,
@@ -437,7 +491,7 @@ class Client:
                    endTime: int = None):
         '''
         GET
-        
+
         To change limit of output orders    -> limit = 1000
         (max value is 1000)
         To use start time and end time      -> startTime = 1573661424937
@@ -457,7 +511,7 @@ class Client:
                        limit: int = 1000):
         '''
         GET
-        
+
         To change limit of output orders    -> limit = 1000
         (max value is 1000)
         '''
@@ -474,7 +528,7 @@ class Client:
         '''
         req = 'listenKey?'
         querystring = urllib.parse.urlencode({'timestamp' : self.timestamp()})
-        
+
         return self._post_request(req, querystring)
 
     def get_listen_key(self):
@@ -486,7 +540,7 @@ class Client:
         '''
         req = 'listenKey?'
         querystring = urllib.parse.urlencode({'timestamp' : self.timestamp()})
-        
+
         return self._put_request(req, querystring)
 
     def close_stream(self):
@@ -495,7 +549,7 @@ class Client:
         '''
         req = 'listenKey?'
         querystring = urllib.parse.urlencode({'timestamp' : self.timestamp()})
-        
+
         return self._delete_request(req, querystring)
-        
+
 #%%%%
